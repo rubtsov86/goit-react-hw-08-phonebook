d4b01d83634e2bec0ff6fecce93c6bdd428efc11
rubtsov86 <rubtsov1986@bigmir.net>
2022-07-04 17:28:59 +0000

    Deploying to gh-pages from @ rubtsov86/goit-react-hw-08-phonebook@fec8a058308a0013e48b5bd92b591733204cc5bc 🚀

--- static/js/home-page.7e007aea.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[223],{6561:function(e,t,a){a.r(t);var r=a(8909),s=a(368),c=a(184);t.default=function(){return(0,c.jsxs)(r.Z,{children:[(0,c.jsx)("h1",{children:"Save your contacts with our solution"}),(0,c.jsx)("img",{src:s,alt:"User avatar"})]})}},368:function(e,t,a){e.exports=a.p+"static/media/image.38ff745269195f7985ae.jpg"}}]);
-//# sourceMappingURL=home-page.7e007aea.chunk.js.map
